3e4ff0d1161a63d45d19a19bc8f155a212f39a33
Sisby folk <sleepingdragoninn@gmail.com>
2026-04-15 20:53:11 +1000

    initial commit

---
+++ src/main/java/dev/sisby/japes/Japes.java
@@ -0,0 +1,15 @@
+package dev.sisby.japes;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Japes implements ModInitializer {
+	public static final String ID = "japes";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("[Japes!] :̶.̶|̶:̶;̶");
+	}
+}
