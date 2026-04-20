c5bf0c3875718b90aff59051eec14cafdc08d344
sisby-folk <sleepingdragoninn@gmail.com>
2026-04-20 16:29:10 +1000

    slightly improved shoelace tying

--- src/main/java/dev/sisby/japes/mixin/MixinEntity.java
+++ src/main/java/dev/sisby/japes/mixin/MixinEntity.java
@@ -16,13 +16,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class MixinEntity {
-	@Inject(method = "interact", at = @At("HEAD"))
+	@Inject(method = "interact", at = @At("HEAD"), cancellable = true)
 	private void tieShoelaces(Player player, InteractionHand hand, Vec3 location, CallbackInfoReturnable<InteractionResult> cir) {
 		if (!((Entity) (Object) this instanceof Player p)) return;
-		if (location.y() < 0.2F) {
+		if (location.y() < 0.3F && player.isCrouching()) {
 			p.addEffect(new MobEffectInstance(MobEffects.SLOWNESS, 100));
 			p.playSound(SoundEvents.UI_CARTOGRAPHY_TABLE_TAKE_RESULT, 0.5F, 0.5F);
 			p.sendOverlayMessage(Component.translatable("message.japes.shoelaces_tied"));
+			cir.setReturnValue(InteractionResult.SUCCESS);
 		}
 	}
 }
