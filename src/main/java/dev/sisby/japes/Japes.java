24f0228305c0a090da6a740271948a4491958ad6
sisby-folk <sleepingdragoninn@gmail.com>
2026-05-04 19:05:00 +1000

    fix game

--- src/main/java/dev/sisby/japes/Japes.java
+++ src/main/java/dev/sisby/japes/Japes.java
@@ -4,13 +4,21 @@ import com.mojang.serialization.Codec;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
+import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Locale;
+import java.util.regex.Pattern;
+
 public class Japes implements ModInitializer {
 	public static final String ID = "japes";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+	Pattern GAME_PATTERN = Pattern.compile("the[^a-z0-9]+game");
 
 	public static final AttachmentType<Boolean> SHOELACES_TIED = AttachmentRegistry.createPersistent(Identifier.fromNamespaceAndPath(Japes.ID, "shoelaces_tied"), Codec.BOOL);
 
@@ -20,6 +28,11 @@ public class Japes implements ModInitializer {
 		JapesEntityTypes.initialize();
 		JapesBlocks.initialize();
 		JapesItems.initialize();
+		ServerMessageEvents.CHAT_MESSAGE.register((message, _, _) -> {
+			if (GAME_PATTERN.matcher(message.signedContent().toLowerCase(Locale.ROOT)).find()) {
+				Minecraft.getInstance().getChatListener().handleOverlay(Component.translatable("message.japes.lost_the_game").withStyle(ChatFormatting.LIGHT_PURPLE));
+			}
+		});
 		LOGGER.info("[Japes!] :̶.̶|̶:̶;̶");
 	}
 }
--- src/main/java/dev/sisby/japes/client/JapesClient.java
+++ src/main/java/dev/sisby/japes/client/JapesClient.java
@@ -2,26 +2,12 @@ package dev.sisby.japes.client;
 
 import dev.sisby.japes.JapesEntityTypes;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
-import net.minecraft.ChatFormatting;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
-import net.minecraft.network.chat.Component;
-
-import java.util.Locale;
-import java.util.regex.Pattern;
 
 public class JapesClient implements ClientModInitializer {
-	Pattern GAME_PATTERN = Pattern.compile("the[^a-z0-9]+game");
-
 	@Override
 	public void onInitializeClient() {
 		EntityRenderers.register(JapesEntityTypes.PAPER_BALL, ThrownItemRenderer::new);
-		ClientReceiveMessageEvents.CHAT.register(((_, playerChatMessage, _, _, _) -> {
-			if (playerChatMessage != null && GAME_PATTERN.matcher(playerChatMessage.decoratedContent().getString().toLowerCase(Locale.ROOT)).find()) {
-				Minecraft.getInstance().getChatListener().handleOverlay(Component.translatable("message.japes.lost_the_game").withStyle(ChatFormatting.LIGHT_PURPLE));
-			}
-		}));
 	}
 }
